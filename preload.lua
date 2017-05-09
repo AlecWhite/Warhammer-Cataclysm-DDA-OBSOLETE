c9af0ab6beab04500e3b85a483e0930fa6b8dee9
AlecWhite <doncuentacuentos@hotmail.com>
2017-05-09 10:03:12 +0200

    Added a 24h cool down between Aquila necklace activations

--- preload.lua
+++ preload.lua
@@ -1,9 +1,19 @@
 
+turn = 0
+
 
 function iuse_for_the_emperor(item, active)
 
-	player.stim = 10
+	current_turn = game:get_calendar_turn():get_turn()
+	
+	if current_turn - turn >= 14400 then
+	player:add_effect(efftype_id("adrenaline"), 100)
 	game.add_msg("You feel blessed by The Emperor!")
+	turn = game:get_calendar_turn():get_turn()
+	else
+	game.add_msg("You need to do more deeds to recieve His blessing")
+	end
+	
 end
 
 --the syntax is
